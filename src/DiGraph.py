45396eb75f7e830c750777e47bec143d71dc477f
aviel780 <aviel780@gmail.com>
2021-12-21 22:27:54 +0200

    finish Digraph

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,66 +1,93 @@
-import json
-import string
-from src.Edge import Edge
-from src.Node import Node
 from src.GraphInterface import GraphInterface
 
 
 class DiGraph(GraphInterface):
-       nodes = {}
-       edges = {}
-       mc = 0
-       def __init__(self) -> None:
-              super().__init__()
-              self.nodes = {}
-              self.edges = {}
-              self.mc = 0
+    def __init__(self) -> None:
+        super().__init__()
+        self.nodes = {}
+        self.edges = {}
+        self.SizeOfEdge = 0
+        self.mc = 0
 
-       def v_size(self) -> int:
-              return len(self.nodes)
+    def v_size(self) -> int:
+        return len(self.nodes)
 
-       def e_size(self) -> int:
-              return len(self.nodes)
+    def e_size(self) -> int:
+        return self.SizeOfEdge
 
-       def get_all_v(self) -> dict:
-              return self.nodes
+    def get_all_v(self) -> dict:
+        return self.nodes
 
-       def all_in_edges_of_node(self, id1: int) -> dict:
-              return self.nodes[id1]
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        ans = {}
+        for s in self.edges.keys():
+            for d in self.edges[s].keys():
+                if d == id1:
+                    if s not in ans.keys():
+                        ans[s] = self.edges[s][id1]
 
-       def all_out_edges_of_node(self, id1: int) -> dict:
-              raise NotImplementedError
+        return ans
 
-       def get_mc(self) -> int:
-              return self.mc
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        return self.edges[id1]
 
-       def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-              temp = Edge(id1, id2, weight)
-              if id1 not in self.edges.keys():
-                     return False
-              if id1 in self.edges.keys() and id2 in self.edges[id1]:# find if id2 is alredy dest in id1
-                     return False
-              self.edges[id1] = temp
-              self.mc = self.mc + 1
-              return True
+    def get_mc(self) -> int:
+        return self.mc
 
-       def add_node(self, node_id: int, pos: tuple = None) -> bool:
-              temp = Node(node_id,pos)
-              if temp.id in self.nodes.keys():
-                     return False
-              self.nodes[node_id]= temp
-              self.edges[node_id] = {}
-              self.mc =self.mc+1
-              return True
+    # id1 - src , id2 - dst
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        if id1 not in self.nodes.keys() or id2 not in self.nodes.keys():
+            return False
 
-       def remove_node(self, node_id: int) -> bool:# need to dalet the edges
-              if node_id not in self.nodes.keys():
-                     return False
-              self.nodes[node_id] = None
+        if id1 in self.edges.keys():
+            if id2 in self.edges[id1].keys():
+                return False
+            else:
+                self.edges[id1][id2] = weight
+                self.SizeOfEdge += 1
+        else:
+            self.edges[id1] = {id2: weight}
+            self.SizeOfEdge += 1
 
-              for i in self.edges.keys():
+        self.mc = self.mc + 1
+        return True
 
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        if node_id in self.nodes.keys():
+            return False
+        else:
+            self.nodes[node_id] = pos
 
+        self.mc = self.mc + 1
+        return True
 
-       def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-              raise NotImplementedError
+    def remove_node(self, node_id: int) -> bool:
+        if node_id not in self.nodes.keys():
+            return False
+        else:
+            # remove from nodes
+            self.nodes.pop(node_id)
 
+        # remove all the edges that the node is the src
+        if node_id in self.edges.keys():
+            self.edges.pop(node_id)
+        # remove all the edges that the node is the dst
+        for id1 in self.edges.keys():
+            for id2 in self.edges[id1].keys():
+                if id2 == node_id:
+                    self.edges[id1].pop(node_id)
+                    self.SizeOfEdge -= 1
+
+        self.mc = self.mc + 1
+        return True
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        if node_id1 not in self.edges.keys():
+            return False
+        if node_id1 in self.edges.keys() and node_id2 not in self.edges[node_id1].keys():
+            return False
+        else:
+            self.edges[node_id1].pop(node_id2)
+            self.SizeOfEdge -= 1
+        self.mc = self.mc + 1
+        return True
