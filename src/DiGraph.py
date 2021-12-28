d8f16ee60af9c44726f22651411aa976b95f8c8d
chen <chenlipy@gmail.com>
2021-12-28 21:41:12 +0200

    final commit

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -4,7 +4,7 @@ from src.Node import Node
 
 
 class DiGraph(GraphInterface):
-    # init the graph
+    # init graph
     def __init__(self) -> None:
         super().__init__()
         self.nodes = {}
@@ -14,32 +14,32 @@ class DiGraph(GraphInterface):
         self.list_edges = []
         self.mc = 0
 
-    # return the size of the nodes
+    # returns the size of the nodes
     def v_size(self) -> int:
         return len(self.nodes)
 
-    # return the size of the edges
+    # returns the size of the edges
     def e_size(self) -> int:
         return self.SizeOfEdge
 
-    # return all the nodes in dicsenery
+    # returns the nodes as a dictionary
     def get_all_v(self) -> dict:
         return self.nodes
 
-    # return all the nodes in list
+    # returns all the nodes in a list
     def get_list_nodes(self) -> list:
         return self.list_nodes
 
-    # return all the edges in list
+    # returns all the edges in a list
     def get_list_edges(self) -> list:
         return self.list_edges
 
-    # return all the nodes thet point to this node
+    # returns all the nodes that point to the given node
     def all_in_edges_of_node(self, id1: int) -> dict:
         ans = {}
-        #going over all the nodes (the key value in edge reprsent the src node)
+        # loop all the nodes (the key value in edge represents the src node)
         for s in self.edges.keys():
-            #going over all the dest nodes
+            # loop all the dest nodes
             for d in self.edges[s].keys():
                 if d == id1:
                     if s not in ans.keys():
@@ -47,23 +47,23 @@ class DiGraph(GraphInterface):
 
         return ans
 
-    # return all the nodes thet the node point at
+    # return all the nodes which the given node is pointing at
     def all_out_edges_of_node(self, id1: int) -> dict:
         return self.edges[id1]
 
-    # return the number of the acsions how did on the graph
+    # return the number of the actions that were made on the graph
     def get_mc(self) -> int:
         return self.mc
 
-    # add edge to the graph
+    # add edge to graph
     # id1 - src , id2 - dst
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        #if dont have the src or the dest in the nodes graph
+        # if id1 or id2 do not exist as nodes in the graph
         if id1 not in self.nodes.keys() or id2 not in self.nodes.keys():
             return False
         edge = Edge(id1, id2, weight)
         if id1 in self.edges.keys():
-            #if the edge allredy vesist in the graph
+            # if the edge already exist in the graph
             if id2 in self.edges[id1].keys():
                 return False
             else:
@@ -71,18 +71,18 @@ class DiGraph(GraphInterface):
                 self.SizeOfEdge += 1
         else:
             edge = Edge(id1, id2, weight)
-            #we create at as dict in dict the key of the first dict is the src node the value is the sconde dict,
-            #in the second dict the key is the dest node and the value is all the permetrs of the edge
+            # we have created two dictionaries, the key valur of the first dict is the src node, the value us the second dict
+            # in the second dict the key is the dest node and the value is all the edge perimeters
             self.edges[id1] = {id2: edge}
             self.SizeOfEdge += 1
-        # save into a list all the permetrs we need to the save file
+        # save into a list all the perimeters we need to the save file
         self.list_edges.append({'src': id1, 'dest': id2, 'w': weight})
         self.mc = self.mc + 1
         return True
 
     # add node to the graph
     def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        # if the node all redy exsist
+        # if the node is already exist
         if node_id in self.nodes.keys():
             return False
         else:
@@ -96,14 +96,14 @@ class DiGraph(GraphInterface):
     def getnode(self, id: int) -> Node:
         return self.nodes[id]
 
-    # remove node frome the graph
+    # remove node from graph
     def remove_node(self, node_id: int) -> bool:
         if node_id not in self.nodes.keys():
             return False
         else:
             # remove from nodes
             self.nodes.pop(node_id)
-        # remove all the edges that the node is the src
+        # remove all the edges that node is their src
         if node_id in self.edges.keys():
             self.edges.pop(node_id)
         for i in self.list_edges:
@@ -114,15 +114,19 @@ class DiGraph(GraphInterface):
         self.mc = self.mc + 1
         return True
 
-    # remove edge from the graph
+    # remove edge from graph
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        # if the node id does not exist
         if node_id1 not in self.edges.keys():
             return False
+        # if id1 exist in src but id2 does not exist in dest
         if node_id1 in self.edges.keys() and node_id2 not in self.edges[node_id1].keys():
             return False
         else:
             self.edges[node_id1].pop(node_id2)
+            # if node id1 = src and node id2 = dest, delete it
             for i in self.list_edges:
+                # delete in case one of the nodes was deleted
                 if i['src'] == node_id1 and i['dest'] == node_id2:
                     self.list_edges.remove(i)
                     break
--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -15,6 +15,7 @@ class GraphAlgo(GraphAlgoInterface):
     def get_graph(self) -> GraphInterface:
         return self.graph
 
+    # load json file
     def load_from_json(self, file_name: str) -> bool:
         with open(file_name, 'r') as f:
             data = json.load(f)
@@ -22,11 +23,13 @@ class GraphAlgo(GraphAlgoInterface):
         l_edges = data["Edges"]
         for dic_nodes in l_nodes:
             pos = dic_nodes["pos"].split(',')
+            # at first save id, second save the position
             self.graph.add_node(dic_nodes['id'], (float(pos[0]), float(pos[1]), float(pos[2])))
         for dic_edges in l_edges:
             self.graph.add_edge(dic_edges['src'], dic_edges['dest'], dic_edges['w'])
         return True
 
+    # save to json file
     def save_to_json(self, file_name: str) -> bool:
         graph_dic = {'Nodes': self.graph.get_list_nodes(), 'Edges': self.graph.get_list_edges()}
         with open(file_name, 'w') as json_file:
@@ -41,8 +44,11 @@ class GraphAlgo(GraphAlgoInterface):
         while len(queue) != 0:
             queue.sort()
             temp = queue.pop(0)
+            # white == was not visited
             if temp.getinfo() == "White":
+                # Black == was visited
                 temp.setinfo("Black")
+                # if all the nodes were visited
                 if temp.getkey() == dest.getkey():
                     return temp.getweight()
                 for edg in self.graph.all_out_edges_of_node(temp.getkey()):
@@ -55,31 +61,37 @@ class GraphAlgo(GraphAlgoInterface):
                         queue.append(tempno)
         return mostshort
 
+    # reset after executing one of the algorithms
     def reset(self):
         for n in self.graph.nodes.values():
             n.setinfo("White")
             n.settag(-1)
             n.setweight(float('inf'))
 
+    # returns a list of all the node which were visited and the distance of all nodes list
     def shortest_path(self, id1: int, id2: int) -> (float, list):
         ans = []
         ansdist = self.shortest_path_dist(id1, id2)
-        if ansdist == -1:  # shode to throw exsepion
+        # -1 -> there is no path between id1 and id2
+        if ansdist == -1:
             return None
         if id1 == id2:
             return (0, [])
         self.reset()
+        # search for the shortest path by changing the tags
         self.Dijkstra(self.graph.getnode(id1), self.graph.getnode(id2))
         nodesrc = self.graph.getnode(id1)
         nodedest = self.graph.getnode(id2)
         back = []
         temp = nodedest
+        # loop as long as the algo didnt get to the start node
         while temp.gettag() != -1:
             back.append(temp)
             temp = self.graph.getnode(temp.gettag())
 
         ans.append(nodesrc.key)
 
+        # enters all the path answers to a list
         for i in range(len(back)-1, -1, -1):
             ans.append(back[i].key)
         self.reset()
@@ -89,10 +101,11 @@ class GraphAlgo(GraphAlgoInterface):
         self.reset()
         ans = self.Dijkstra(self.graph.get_all_v().get(src), self.graph.get_all_v().get(dest))
         self.reset()
-        if ans == 1000000:
+        if ans == float('inf'):
             return -1
         return ans
 
+    # returns a list of the nodes id's in the path, and the overall distance
     def TSP(self, node_lst: List[int]) -> (List[int], float):
         list_ans = []
         p = []
@@ -102,45 +115,53 @@ class GraphAlgo(GraphAlgoInterface):
             sec.append(n)
         firstvalue = sec.pop(0)
         while len(sec) != 0:
-            best = 1000000
+            best = float('inf')
             for nod in sec:
                 temp = self.shortest_path_dist(firstvalue, nod)
                 if temp < best:
                     best = temp
                     Ntemp = nod
             p = self.shortest_path(firstvalue, Ntemp)
-            p =  p[1]
-            for i in range(1, len( p), 1):
-                list_ans.append( p[i])
+            p = p[1]
+            for i in range(1, len(p), 1):
+                list_ans.append(p[i])
             sec.remove(Ntemp)
             firstvalue = Ntemp
+            # computes the distance between all vertices
         for a in range(0, len(list_ans)-1, 1):
             temp = self.graph.get_edge(list_ans[a],list_ans[a+1]).getweight()
-            ans = ans +temp
+            ans = ans+temp
         return list_ans,ans
 
+    # Finds the node that has the shortest distance to it's farthest node.
+    # returns the nodes id, min-maximum distance
     def centerPoint(self) -> (int, float):
         c = 0
-        ford = Node(-1, (0, 0, 0))
-        ansdist = float('inf')
+        for_d = Node(-1, (0, 0, 0))
+        ans_dist = float('inf')
         for nod in self.graph.nodes.values():
             self.reset()
-            self.Dijkstra(nod, ford)
+            # creates path between each node in the graph
+            self.Dijkstra(nod, for_d)
             temp = float('-inf')
             for tempn in self.graph.nodes.values():
+                # finds the node with the highest weight
                 if tempn.getweight() > temp:
                     temp = tempn.getweight()
-            if temp < ansdist:
-                ansdist = temp
+            if temp < ans_dist:
+                ans_dist = temp
                 c = nod.key
-        return c, ansdist
+        return c, ans_dist
 
+    # shows the graph as GUI, (x,y) coordinates, z->height =0
     def plot_graph(self) -> None:
         for src in self.graph.nodes.values():
             x, y, z = src.pos
+            # node-> green, node id-> red
             plt.plot(x, y, markersize=10, marker="o", color="green")
             plt.text(x, y, str(src.key), color="red", fontsize=14)
-
+            # place nodes according to the locations
+            # add arrows
             for dest in self.graph.edges[src.key]:
                 n_x, n_y, n_z = self.graph.nodes[dest].pos
                 plt.annotate("", xy=(x, y), xytext=(n_x, n_y), arrowprops=dict(arrowstyle="<-"))
